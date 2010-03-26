3b06926e33b7ded86a43149938ab41e71b8d929c
Jason LaPier <jason.lapier@gmail.com>
2010-03-26 16:21:02 -0700

    fixed stylesheet tags and page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,18 @@
 module ApplicationHelper
 
   def site_title
-    "Test"
+    "Contact and Event Tracker"
   end
 
   def page_title
-    "Test"
+    if controller.controller_name == "user_sessions"
+      pt = ''
+    else
+      pt = "#{controller.controller_name.humanize}"
+      pt += " - #{@contact.name}" if @contact
+      pt += " - #{@contact_group.name}" if @contact_group
+      pt += " - #{@event.name}" if @event
+    end
+    pt
   end
 end
