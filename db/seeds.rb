6ab38deac1cc6f668c6f92b5876f2acdc0f2aab0
Jason LaPier <jason.lapier@gmail.com>
2010-03-29 09:18:35 -0700

    css tweak

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,15 @@
 
 require 'csv'
 
-CSV.open('db/pac-emails.csv', 'r') do |row|
-  Contact.create :first_name => row[1], :last_name => row[2], :email => row[3]
+contacts = []
+
+CSV.open('/home/jason/rails/pac_rti/contact-and-event-tracker/db/pac-emails-new.csv', 'r') do |row|
+  #Contact.create :first_name => row[1], :last_name => row[2], :email => row[3]
+  #contacts << Contact.find( :first, :conditions => { :first_name => row[1], :last_name => row[2] }  )
+  contact = Contact.find( :first, :conditions => { :first_name => row[0], :last_name => row[1] }  )
+  contact.update_attributes :title => row[2], :division => row[3], :agency => row[4], :city => row[5],
+    :state => row[6], :zip => row[7], :agency_phone => row[8], :direct_phone => row[9],
+    :home_phone => row[10], :fax_phone => row[11], :website => row[12], :street_address => row[13],
+    :comments => row[14], :descriptors => row[15], :home_address => row[16]
 end
 
