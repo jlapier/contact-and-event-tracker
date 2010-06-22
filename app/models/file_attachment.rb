0000acccfb9e82db96fd9e5e314c19589aa09a99
Jeremiah Heller <jeremiah@inertialbit.net>
2010-06-22 12:46:38 -0700

    provide file listing & uploads from root_path w/ download links
    - refactored new file attachment form from events/show to upload_form

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -1,3 +1,10 @@
+# t.string   "name"
+# t.text     "description"
+# t.string   "filepath"
+# t.integer  "event_id"
+# t.datetime "created_at"
+# t.datetime "updated_at"
+
 class FileAttachment < ActiveRecord::Base
   validates_presence_of :name, :filepath
   
