536582bbe91ffbc02a48892a71f763f6230e0c03
Jeremiah Heller <jeremiah@inertialbit.net>
2010-06-22 18:47:20 -0700

    integrate plupload to the root_path for multiple file uploads
    - refactor file attachments create routine to model

--- app/models/file_attachment.rb
+++ app/models/file_attachment.rb
@@ -6,10 +6,60 @@
 # t.datetime "updated_at"
 
 class FileAttachment < ActiveRecord::Base
+  
+  FOLDER = 'files'
+  FOLDER_ROOT = File.join(RAILS_ROOT, 'public')
+  FOLDER_PATH = File.join(FOLDER_ROOT, FOLDER)
+  
   validates_presence_of :name, :filepath
   
   belongs_to :event
+  
+  before_validation_on_create :autofill_blank_name
+  before_validation_on_create :save_to_folder_path
+  # before_validation_on_update :autofill_blank_name, :if => :file_uploaded?
+  # before_validation_on_update :save_to_folder_path, :if => :file_uploaded?
 
   attr_accessor :uploaded_file
   
+  private
+    def save_to_folder_path
+      build_filepath
+      File.open(full_path, "wb") { |f| f.write(uploaded_file.read) }
+      return true if file_saved?
+      errors.add_to_base("The file could not be saved. Please try again or contact someone and make a note of how many files, what kind, etc.")
+      false
+    end
+    def generate_unique_filename
+      base_filename = File.basename uploaded_file.original_filename
+      new_filename = base_filename
+      path = File.join FOLDER_PATH, new_filename
+      count = 0
+      until !File.exists? path
+        count += 1
+        new_filename = base_filename.gsub File.extname(base_filename), "-#{count}#{File.extname(base_filename)}"
+        path = File.join FOLDER_PATH, new_filename
+      end
+      new_filename
+    end
+    def ensure_folder_path_exists
+      FileUtils.mkdir_p FOLDER_PATH
+    end
+    def build_filepath
+      self.filepath = File.join FOLDER, generate_unique_filename
+    end
+    def autofill_blank_name
+      if name.blank?
+        self.name = self.uploaded_file.original_filename
+      end
+    end
+  protected
+  public
+    def full_path
+      "#{FOLDER_ROOT}/#{filepath}"
+    end
+    def file_saved?
+      return true if File.exists?(full_path) && File.basename(full_path) != FOLDER
+      return false
+    end
 end
--- spec/models/file_attachment_spec.rb
+++ spec/models/file_attachment_spec.rb
@@ -1,16 +1,46 @@
 require 'spec_helper'
 
 describe FileAttachment do
+  
+  include ActionController::TestProcess # for fixture_file_upload
+  
   before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :description => "value for description",
-      :filepath => "value for filepath",
-      :event_id => 1
-    }
+    @path = File.join(RAILS_ROOT, 'public', 'files')
+    @full_path = File.join(@path, 'somefile.txt')
+    
+    raise "Please back up and clean out public/files before running this spec" if File.exists?(@full_path) || File.exists?(File.join(@path, 'somefile-1.txt'))
+    
+    @file_attachment = FileAttachment.create!({
+      :description => 'unique description',
+      :uploaded_file => fixture_file_upload("somefile.txt", 'text/plain')
+    })
   end
-
-  it "should create a new instance given valid attributes" do
-    FileAttachment.create!(@valid_attributes)
+  
+  after(:each) do  
+    if File.exists?(@full_path)
+      File.delete(@full_path)
+    end
+  end
+  
+  it "should generate a unique name" do
+    FileUtils.mkdir_p @path
+    
+    new_file = FileAttachment.create!({
+      :description => 'other description',
+      :uploaded_file => fixture_file_upload("somefile.txt", 'text/plain')
+    })
+    
+    new_file.filepath.should_not == 'files/somefile.txt'
+    new_file.filepath.should == 'files/somefile-1.txt'
+    File.exists?(@full_path).should be_true
+    
+    File.delete(File.join(@path, 'somefile-1.txt'))
+  end
+  
+  it "should know whether its file actually exists" do
+    @file_attachment.file_saved?.should be_true
+    File.exists?(@full_path).should be_true
+    File.delete(@full_path)
+    @file_attachment.file_saved?.should be_false
   end
 end
