c6d7c7be4da17459998022adf594f7b6c8318a0b
Jeremiah Heller <jeremiah@inertialbit.net>
2010-06-25 07:20:56 +0800

    make false the default for users.is_admin

---
+++ db/migrate/20100624231511_make_false_default_for_user_is_admin.rb
@@ -0,0 +1,9 @@
+class MakeFalseDefaultForUserIsAdmin < ActiveRecord::Migration
+  def self.up
+    change_column :users, :is_admin, :boolean, :default => false
+  end
+
+  def self.down
+    change_column :users, :is_admin, :boolean
+  end
+end
