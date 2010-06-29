0ac40d15add9a0d4922892449795cb9f075da1b5
Jeremiah Heller <jeremiah@inertialbit.net>
2010-06-29 09:50:36 +0800

    delete files from the database & move them to a trash folder on the filesystem

--- spec/controllers/file_attachments_controller_spec.rb
+++ spec/controllers/file_attachments_controller_spec.rb
@@ -21,6 +21,12 @@ describe FileAttachmentsController do
   def some_file
     fixture_file_upload("somefile.txt", 'text/plain')
   end
+  
+  def it_redirects_back(&block)
+    session[:return_to] = 'http://example.com/contacts'
+    yield
+    response.should redirect_to('http://example.com/contacts')
+  end
 
   describe "when logged in as admin" do
     before do
@@ -38,8 +44,8 @@ describe FileAttachmentsController do
         response.should redirect_to(event_url(mock_event, :std => 1))
       end
       it "should upload a new file attachment without an event" do
-        @params.delete(:event_id)
-        mock_file = mock_model(FileAttachment, @params.merge({:save => nil, :event => mock_event, :errors => mock('Error', {:full_messages => []})}))
+        @params.merge!(:event_id => '')
+        mock_file = mock_model(FileAttachment, @params.merge({:save => nil, :event => '', :errors => mock('Error', {:full_messages => []})}))
         FileAttachment.should_receive(:new).and_return(mock_file)
         post :create, :file_attachment => @params
       end
@@ -49,7 +55,7 @@ describe FileAttachmentsController do
       end
     end
     
-    context "xhr upload" do
+    context "plupload" do
       
       before(:each) do
         @params.delete(:name) && @params.delete(:description)
@@ -58,7 +64,7 @@ describe FileAttachmentsController do
       it "should upload a new file attachment with an event" do
         FileAttachment.should_receive(:new).and_return(mock_file_attachment)
         
-        xhr :post, :create, {
+        post :create, {
           :file => some_file,
           :event_id => mock_event.id
         }
@@ -69,14 +75,80 @@ describe FileAttachmentsController do
       end
       
       it "should upload a new file attachment without an event" do
-        xhr :post, :create, {
+        post :create, {
           :file => some_file
         }
         
         assigns[:file_attachment].should == mock_file_attachment
       end
     end
+    
+    context "delete a file" do
+      
+      before(:each) do
+        controller.stub(:current_user).and_return(mock_admin_user)
+        FileAttachment.stub(:find).and_return(mock_file_attachment)
+        mock_file_attachment.stub(:full_path).and_return(File.join(RAILS_ROOT, 'spec', 'fixtures', 'somefile.txt'))
+        mock_file_attachment.stub(:destroy).and_return(mock_file_attachment)
+        File.stub(:rm)
+      end
+      
+      it "load the file attachment" do
+        FileAttachment.should_receive(:find).and_return(mock_file_attachment)
+        post :destroy, :id => 1
+        assigns[:file_attachment].should == mock_file_attachment
+      end
+      
+      it "destroys the file attachment" do
+        mock_file_attachment.should_receive(:destroy)
+        post :destroy, :id => 1
+      end
+      
+      it "sets a flash[:notice]" do
+        post :destroy, :id => 1
+        flash[:notice].should_not be_nil
+      end
+      
+      it "redirects back" do
+        it_redirects_back{ post :destroy, :id => 1 }
+      end
+      
+      it "OR to root_path" do
+        post :destroy, :id => 1
+        response.should redirect_to('http://test.host/') # < wtf?!
+      end
+      
+    end
 
+  end
+  
+  describe "when Errno::ENOENT is raised" do
+    
+    before(:each) do
+      controller.stub(:current_user).and_return(mock_admin_user)
+      FileAttachment.stub(:find).and_return(mock_file_attachment)
+      mock_file_attachment.stub(:destroy).and_raise(Errno::ENOENT.new("File not found"))
+    end
+    
+    it "logs the error" do
+      controller.logger.should_receive(:error).with("FileAttachmentsController[destroy] was rescued with :file_not_found. No such file or directory - File not found")
+      post :destroy, :id => 1
+    end
+    
+    it "sets a flash[:warning]" do
+      post :destroy, :id => 1
+      flash[:warning].should_not be_nil
+    end
+    
+    it "redirects back" do
+      it_redirects_back{ post :destroy, :id => 1 }
+    end
+    
+    it "OR to root_path" do
+      post :destroy, :id => 1
+      response.should redirect_to('http://test.host/')
+    end
+    
   end
   
   describe "when visiting anonymously" do
