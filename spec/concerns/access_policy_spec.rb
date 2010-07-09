4f6a913082bc6a50ff86ba9f20264417583c8f5d
Jeremiah Heller <jeremiah@inertialbit.net>
2010-07-09 01:27:02 -0700

    verify negatives

--- spec/concerns/access_policy_spec.rb
+++ spec/concerns/access_policy_spec.rb
@@ -24,13 +24,11 @@ def privileges
   }
 end
 
-describe AccessPolicy do
-  
+def non_admin_roles
+  ['editor', 'general']
+end
 
-  
-  before(:each) do
-    #User.stub(:roles).and_return(privileges)
-  end
+describe AccessPolicy do
   
   it "should be able to identify matches given either symbols or strings" do
     AccessPolicy.these_match?(:some_value, 'some_value').should be_true
@@ -43,6 +41,30 @@ describe AccessPolicy do
     
     it "admin is always allowed all privileges" do
       AccessPolicy.allows?('admin', 'to_do', 'anything')
+    end
+    
+    describe "exclusively allows Admin access to Users; " do
+      non_admin_roles.each do |role|
+        describe "#{role.capitalize} users are prohibited from:" do
+          %w(index show create edit update).each do |action|
+            it action do
+              AccessPolicy.allows?(role, action, 'users').should be_false
+            end
+          end
+        end
+      end
+    end
+    
+    describe "prohibit General Users from the following access to" do
+      %w(events contacts contact_groups file_attachments).each do |resource|
+        describe resource do
+          %w(new create edit update delete).each do |action|
+            it action do
+              AccessPolicy.allows?('general', action, resource).should be_false
+            end
+          end
+        end
+      end
     end
     
     %w(editor general).each do |role|
@@ -102,39 +124,24 @@ describe AccessPolicy do
                 end if verb.to_s =~ AccessPolicy.deletable_regex
               end # end describe "for #{controller.to_s.split('_')...}"
             end if targets.kind_of?(Array)
-
-            #AccessPolicy.writable_regex.to_s.delete('()/').gsub(/^\?i-mx:/, '').split('|').flatten.compact.each do |action|
-
-                case targets
-                when :all || 'all'
-                  it "#{verb} access for all resources" do
-                    AccessPolicy.allows?(role, verb, 'all').should be_true
-                  end
-                when :none || 'none'
-                  it "denied #{verb} access from any resources" do
-                    AccessPolicy.allows?(role, verb, 'any').should be_false
-                  end
-                else
-                  it "#{targets} is unknown: all and none are the only valid options if controller names aren't passed in an Array" do
-                    (targets.to_s =~ /(all|none)/).should be_true
-                  end
-                end if targets.kind_of?(Symbol)
-
-            #end 
+            
+            case targets
+            when :all || 'all'
+              it "#{verb} access for all resources" do
+                AccessPolicy.allows?(role, verb, 'all').should be_true
+              end
+            when :none || 'none'
+              it "denied #{verb} access from any resources" do
+                AccessPolicy.allows?(role, verb, 'any').should be_false
+              end
+            else
+              it "#{targets} is unknown: all and none are the only valid options if controller names aren't passed in an Array" do
+                (targets.to_s =~ /(all|none)/).should be_true
+              end
+            end if targets.kind_of?(Symbol)
           end
         end
       end
-      end
     end
-  
-  # context "admin" do
-  #   it "should always be allowed" do
-  #     AccessPolicy.allows?('admin', :any_privilege, :any_controller).should be_true
-  #   end
-  #   it "should always be granted privileges" do
-  #     AccessPolicy.role_granted_privilege?(privileges, 'random_privilege', 'some_new_controller')
-  #   end
-  # end
-  # 
-  
+  end
 end
