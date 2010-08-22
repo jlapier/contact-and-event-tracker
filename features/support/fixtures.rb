80e831f59e4e84e0211a8f34484f8f25bf04cbc1
Jeremiah Heller <jeremiah@inertialbit.net>
2010-08-22 11:07:06 -0700

    load fixtures in Before block to allow for running all integration tests at once

--- features/support/fixtures.rb
+++ features/support/fixtures.rb
@@ -1,5 +1,7 @@
 #Seed the DB
-Fixtures.reset_cache  
-fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
-fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-Fixtures.create_fixtures(fixtures_folder, fixtures)
+Before do
+  Fixtures.reset_cache  
+  fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
+  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+  Fixtures.create_fixtures(fixtures_folder, fixtures)
+end
